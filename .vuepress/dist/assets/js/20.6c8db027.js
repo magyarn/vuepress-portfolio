5e6bc909bcb314048614edaf8c7ea4e56332447e
Nathan Magyar <magyarn@umich.edu>
2019-03-07 14:21:06 -0500

    Build new updates

--- .vuepress/dist/assets/js/20.6c8db027.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{224:function(t,e,n){"use strict";n.r(e);var a=n(0),i=Object(a.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("ArticlePage",{attrs:{article:this.$page.frontmatter}})],1)},[],!1,null,null,null);i.options.__file="what-are-you-thinking.md";e.default=i.exports}}]);
